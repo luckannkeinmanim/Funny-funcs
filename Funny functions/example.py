de413c080bffbd9b6e0c64c8606f0699a12caf62
PurplxWasTaken <silvertandluccc@gmail.com>
2026-04-03 02:04:32 +0200

    Update function calls and user interaction in example.py
    Refactor function calls to use 'f' alias and enhance interaction.

--- Funny functions/example.py
+++ Funny functions/example.py
@@ -1,3 +1,18 @@
-import funfuncs as ff # or only import funfuncs whatever you prefer
+import funfuncs as f
 
-say("Hello World!")
+f.help() 
+
+f.say('thanks for checking this before using i hope.')
+
+# Get the name
+name = f.get('whats your name? ')
+
+# Get the age and turn it into a number (int)
+# We use an f-string to combine the name into the question
+age_input = f.get(f'hi {name}, how old are you? ')
+age = int(age_input)
+
+if age >= 18:
+    f.say(age, '? oh alright unc')
+else:
+    f.say('knowing python at', age, 'is quite impressive lil bro')
